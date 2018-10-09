d4b9f1cc1db24e226ba4ec76cf55f18ed0abdf8f
chenzhiji <767042953@qq.com>
2018-10-09 19:57:22 +0800

    count_islands 99%

--- Queue&Stack/count_islands/count_islands.cpp
+++ Queue&Stack/count_islands/count_islands.cpp
@@ -3,12 +3,14 @@
 //
 #include "iostream"
 #include "queue"
+#include "vector"
 
 using namespace std;
 
 typedef struct {
     int x;
     int y;
+    bool visited;
 }pointPos;
 
 class Solution {
@@ -21,34 +23,37 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         pointPos now_point, next_point;
         int i,j;
+        now_point.visited = false;
+        next_point.visited = false;
         for(i=0; i<grid.size(); i++) {
             for (j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 1) {
+                if (grid[i][j] == '1') {
                     now_point.x = i;
                     now_point.y = j;
                     unvisited.push_back(now_point);
                 }
             }
         }
-        now_point.x = unvisited.begin().operator*().x;
-        now_point.y = unvisited.begin().operator*().y;
-        q.push(now_point);
-        unvisited.erase(unvisited.begin());
-        while(!unvisited.empty()){
+        if(!unvisited.empty()) {
+            now_point.x = unvisited[0].x;
+            now_point.y = unvisited[0].y;
+            unvisited[0].visited = true;
+            q.push(now_point);
+        }
+        else
+            return num;
+        int flg=1;
+        while(flg){
             for(i = 0; i < 4; i++)
             {
-                next_point.x = now_point.x + step[0][0];
-                next_point.y = now_point.y + step[0][1];
+                next_point.x = now_point.x + step[i][0];
+                next_point.y = now_point.y + step[i][1];
                 if(next_point.x >= 0 && next_point.x < grid.size() && next_point.y >= 0 && next_point.y < grid[0].size()){
-                    if(grid[next_point.x][next_point.y] == 1) {
-                        q.push(next_point);
-                        for(i = 0; i<unvisited.size();i++)
-                        {
-                            if(unvisited.begin().operator*().x == next_point.x && unvisited[i].y == next_point.y) {
-                                if(i == unvisited.size() - 1) {
-                                    unvisited.erase(unvisited.begin() + i);
-                                    unvisited.end() = unvisited.begin();
-                                }
+                    if(grid[next_point.x][next_point.y] == '1') {
+                        for(j = 0; j<unvisited.size();j++) {
+                            if(next_point.x == unvisited[j].x && next_point.y == unvisited[j].y && !unvisited[j].visited){
+                                unvisited[j].visited = true;
+                                q.push(next_point);
                             }
                         }
                     }
@@ -57,14 +62,22 @@ public:
             q.pop();
             if(!q.empty()) {
                 now_point.x = q.front().x;
-                next_point.x = q.front().y;
+                now_point.y = q.front().y;
             }
             else {
                 num += 1;
-                if(!unvisited.empty()) {
-                    now_point.x = unvisited.begin().operator*().x;
-                    now_point.y = unvisited.begin().operator*().y;
+                flg = 0;
+                for(i = 0;i < unvisited.size();i++){
+                    if(!unvisited[i].visited) {
+                        now_point.x = unvisited[i].x;
+                        now_point.y = unvisited[i].y;
+                        unvisited[i].visited = true;
+                        q.push(now_point);
+                        flg = 1;
+                        break;
+                    }
                 }
+
             }
         }
         return num;
